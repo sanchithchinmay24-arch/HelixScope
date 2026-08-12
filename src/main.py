bb0449f1e3ab3a0fefc07da7ba7d4e510d31b9b3
Sanchith Chinmaay Harish <sanchithchinmay24@gmail.com>
2026-08-12 15:53:29 +0530

    feat: validate and normalize DNA sequences

--- src/main.py
+++ src/main.py
@@ -1,22 +1,34 @@
-dna = "AAAAAAAAAA"
+def analyze_dna(dna):
+    dna = dna.upper()
 
-length = len(dna)
+    valid_bases = {"A", "T", "G", "C"}
 
-a_count = dna.count("A")
-t_count = dna.count("T")
-g_count = dna.count("G")
-c_count = dna.count("C")
+    if not set(dna).issubset(valid_bases):
+        print("Invalid DNA sequence")
+        return
 
-gc_content = (g_count + c_count) / length * 100
+    length = len(dna)
+    
+    a_count = dna.count("A")
+    t_count = dna.count("T")
+    g_count = dna.count("G")
+    c_count = dna.count("C")
 
-print("DNA sequence:", dna)
-print("Sequence length:", length)
-print("A:", a_count)
-print("T:", t_count)
-print("G:", g_count)
-print("C:", c_count)
-print("GC content:", gc_content, "%")
-at_content = (a_count + t_count) / length * 100
-print("AT content:", at_content, "%")
-total_content = at_content + gc_content
-print("Total AT + GC:", total_content, "%")
+    gc_content = (g_count + c_count) / length * 100
+    at_content = (a_count + t_count) / length * 100
+    total_content = at_content + gc_content
+
+    print("DNA sequence:", dna)
+    print("Sequence length:", length)
+    print("A:", a_count)
+    print("T:", t_count)
+    print("G:", g_count)
+    print("C:", c_count)
+    print("GC content:", gc_content, "%")
+    print("AT content:", at_content, "%")
+    print("Total AT + GC:", total_content, "%")
+
+analyze_dna("ATGCGGTAAC")
+analyze_dna("AAAAAAAAAA")
+analyze_dna("ATGCGXTAAC")
+analyze_dna("atgcggtaac")
